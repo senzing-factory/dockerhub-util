641765cb1d4c7003179c8e5f4484e1798dc9d0c1
michael@dockter.com <michael@dockter.com>
2021-07-22 16:54:43 -0400

    #16 Prepare for versioning

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,15 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 [markdownlint](https://dlaa.me/markdownlint/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.0.2] - 2021-07-22
+
+### Changed in 1.0.2
+
+- Change base to `debian:10.10`
+- Use `requirements.txt`
+- Fix Semantic Versioning comparison
+- Add `SENZING_DOCKER_IMAGE_VERSION_*` variables
+
 ## [1.0.1] - 2021-04-09
 
 ### Changed in 1.0.1
--- dockerhub-util.py
+++ dockerhub-util.py
@@ -17,9 +17,9 @@ from datetime import date
 from packaging.version import Version
 
 __all__ = []
-__version__ = "1.0.1"  # See https://www.python.org/dev/peps/pep-0396/
+__version__ = "1.0.2"  # See https://www.python.org/dev/peps/pep-0396/
 __date__ = '2021-02-22'
-__updated__ = '2021-07-19'
+__updated__ = '2021-07-22'
 
 SENZING_PRODUCT_ID = "5018"  # See https://github.com/Senzing/knowledge-base/blob/master/lists/senzing-product-ids.md
 log_format = '%(asctime)s %(message)s'
@@ -184,7 +184,7 @@ dockerhub_repositories_for_latest = {
     'x-ibmcom-db2': {
         'environment_variable': 'SENZING_DOCKER_IMAGE_VERSION_IBMCOM_DB2',
         'image': 'ibmcom/db2',
-        'version': '11.5.5.1'
+        'version': '11.5.6.0'
     },
     'x-kafdrop': {
         'environment_variable': 'SENZING_DOCKER_IMAGE_VERSION_OBSIDIANDYNAMICS_KAFDROP',
